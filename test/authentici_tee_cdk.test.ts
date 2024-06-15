ba05e33ff92140c575005ca099f81e6d5dc27fb8
JHERR1112 <herrera.71.1112@gmail.com>
2024-06-15 14:04:37 -0400

    Initial commit

---
+++ test/authentici_tee_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AuthenticiTeeCdk from '../lib/authentici_tee_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/authentici_tee_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AuthenticiTeeCdk.AuthenticiTeeCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
